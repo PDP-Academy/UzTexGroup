ecd38134426ce9977003fc71f030345907fd4dee
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2023-01-24 18:28:20 +0500

    dbContext has been added

---
+++ UzTexGroup.Infrastructure/Configurations/UsersConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UzTexGroup.Domain.Entities;
+
+namespace UzTexGroup.Infrastructure.Configurations;
+
+public class UsersConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+       
+    }
+}
