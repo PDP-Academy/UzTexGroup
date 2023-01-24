58b31474325524ccbd896784ca6bad203caf10c6
muxam <muxammadyorkenjayev21@gmail.com>
2023-01-24 15:45:42 +0500

    Added AboutCompanies model

---
+++ UzTexGroup.Domain/Entities/AboutCompanies.cs
@@ -0,0 +1,10 @@
+﻿namespace UzTexGroup.Domain.Entities
+{
+    public class AboutCompanies
+    {
+        public Guid Id { get; set; }
+        public string TitleUz { get; set; }
+        public string image_Url { get; set; }
+        public string AboutCompanyUz { get; set; }
+    }
+}
