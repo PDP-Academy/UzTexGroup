006ba66b7539f61b3fc2c808d694b60192b425fc
Jasurbek <mamatqulovjasurbek805@gmail.com>
2023-01-24 22:18:46 +0500

    Add Configuration

--- UzTexGroup.Domain/Entities/News.cs
+++ UzTexGroup.Domain/Entities/News.cs
@@ -2,9 +2,9 @@
 public class News
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
     public DateTime Date { get; set; }
     public string Title { get; set; }
     public string Body { get; set; }
+    public ICollection<NewImages>? NewImages { get; set; }
 
 }
---
+++ UzTexGroup.Infrastructure/Configurations/AddressConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UzTexGroup.Domain.Constants;
+using UzTexGroup.Domain.Entities;
+
+namespace UzTexGroup.Infrastructure.Configurations;
+public class AddressConfiguration : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.ToTable(TableNames.Address);
+
+        builder.HasKey(address => address.Id);
+
+        builder.Property(address => address.Country)
+            .IsRequired()
+            .HasMaxLength(50);
+
+         builder.Property(address => address.Region)
+            .IsRequired()
+            .HasMaxLength(50);
+
+         builder.Property(address => address.District)
+            .IsRequired()
+            .HasMaxLength(50);
+
+         builder.Property(address => address.Street)
+            .IsRequired()
+            .HasMaxLength(50);
+
+         builder.Property(address => address.PostalCode)
+           .IsRequired();
+    }
+}
