b8d856d9dc03f1ca0823fa87c10ffab7488658e3
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-24 21:10:09 +0500

    added About Company  to Configurations in Infrastructure

---
+++ UzTexGroup.Infrastructure/Configurations/AboutCompaniesConfigurations.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using UzTexGroup.Domain.Constants;
+using UzTexGroup.Domain.Entities;
+
+namespace UzTexGroup.Infrastructure.Configurations;
+public sealed class AboutCompaniesConfigurations : IEntityTypeConfiguration<AboutCompanies>
+{
+    public void Configure(EntityTypeBuilder<AboutCompanies> builder)
+    {
+        builder.ToTable(TableNames.AboutCompanies);
+
+        builder.HasKey(aboutCompanies => aboutCompanies.Id);
+
+        builder
+            .Property(aboutCompanies => aboutCompanies.TitleUz)
+            .HasMaxLength(70)
+            .IsRequired(true);
+
+        builder
+            .Property(aboutCompanies => aboutCompanies.image_Url)
+            .HasMaxLength(255)
+            .IsRequired(true);
+
+        builder
+            .Property(aboutCompanies => aboutCompanies.AboutCompanyUz)
+            .HasMaxLength(500)
+            .IsRequired(true);
+    }
+}
