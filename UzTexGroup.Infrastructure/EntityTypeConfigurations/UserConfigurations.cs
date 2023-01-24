d3202b0d809c0a5ee43651e1a198148cd3b5b0a7
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-24 19:10:34 +0500

    User, Company, AboutCompanies configures added Infrastruct layer

---
+++ UzTexGroup.Infrastructure/EntityTypeConfigurations/UserConfigurations.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UzTexGroup.Domain.Constants;
+using UzTexGroup.Domain.Entities;
+
+namespace UzTexGroup.Infrastructure.EntityTypeConfigurations;
+public sealed class UserConfigurations : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable(TableNames.Users);
+
+        builder.HasKey(user => user.Id);
+
+        builder
+            .Property(user => user.FullName)
+            .HasMaxLength(50)
+            .IsRequired(true);
+
+        builder
+            .Property(user => user.Email)
+            .HasMaxLength(255)
+            .IsRequired(true);
+
+        builder
+            .Property(user => user.PasswordHash)
+            .HasMaxLength(255)
+            .IsRequired(true);
+
+        builder
+            .Property(user => user.Salt)
+            .HasMaxLength(100)
+            .IsRequired(true);
+    }
+}
