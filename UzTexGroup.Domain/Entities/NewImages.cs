570ced1f80c2c4f89e52318167a42d3e59b7fdcd
Jasurbek <mamatqulovjasurbek805@gmail.com>
2023-01-24 16:41:58 +0500

    Add new

---
+++ UzTexGroup.Domain/Entities/NewImages.cs
@@ -0,0 +1,7 @@
+﻿namespace UzTexGroup.Domain.Entities;
+public class NewImages
+{
+    public Guid Id { get; set; }
+    public string image_url { get; set; }
+    public Guid NewsId { get; set; }
+}
