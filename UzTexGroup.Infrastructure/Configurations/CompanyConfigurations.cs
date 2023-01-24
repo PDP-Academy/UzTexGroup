d83a244fee26ae371801e25f48cc181bddb1f41d
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-24 21:10:28 +0500

    added Company Configurations to Infrastructure layer

---
+++ UzTexGroup.Infrastructure/Configurations/CompanyConfigurations.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using UzTexGroup.Domain.Constants;
+using UzTexGroup.Domain.Entities;
+
+namespace UzTexGroup.Infrastructure.Configurations;
+public sealed class CompanyConfigurations : IEntityTypeConfiguration<Company>
+{
+    public void Configure(EntityTypeBuilder<Company> builder)
+    {
+        builder.ToTable(TableNames.Company);
+
+        builder.HasKey(company => company.Id);
+
+        builder
+            .Property(company => company.CompanyName)
+            .HasMaxLength(100)
+            .IsRequired(true);
+
+        builder
+            .Property(company => company.FoundedDate)
+            .IsRequired(true);
+
+        builder
+            .HasOne(company => company.AboutCompany)
+            .WithOne()
+            .HasForeignKey<Company>(company => company.AboutCompanyId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
+
