59f2ed8d2c63a3762962af3fa7ad69fbc242e819
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-24 19:10:34 +0500

    User, Company, AboutCompanies configures added Infrastruct layer

---
+++ UzTexGroup.Infrastructure/EntityTypeConfigurations/CompanyConfigurations.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UzTexGroup.Domain.Constants;
+using UzTexGroup.Domain.Entities;
+
+namespace UzTexGroup.Infrastructure.EntityTypeConfigurations;
+public sealed class CompanyConfigurations : IEntityTypeConfiguration<Company>
+{
+    public void Configure(EntityTypeBuilder<Company> builder)
+    {
+        builder.ToTable(TableNames.Company);
+
+        builder.HasKey(company => company.Id);
+
+        builder
+            .Property(company => company.CompanyName)
+            .HasMaxLength(100)
+            .IsRequired(true);
+
+        builder
+            .Property(company => company.FoundedDate)
+            .IsRequired(true);
+
+        builder
+            .HasOne(company => company.AboutCompany)
+            .WithOne()
+            .HasForeignKey<Company>(company => company.AboutCompanyId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
