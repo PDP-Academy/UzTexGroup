30be982611f7bab09b72692b056118a191e71768
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2023-01-24 21:42:49 +0500

    changed

---
+++ UzTexGroup.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UzTexGroup.Infrastructure.DbContexts;
+
+namespace UzTexGroup.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDbContexts(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("SqlServer");
+
+        services.AddDbContextPool<UzTexGroupDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString, sqlServerOptions =>
+            {
+                sqlServerOptions.EnableRetryOnFailure();
+            });
+        });
+
+        return services;
+    }
+}
---
+++ UzTexGroup.Infrastructure/Configurations/JobConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UzTexGroup.Domain.Entities;
+
+namespace UzTexGroup.Infrastructure.Configurations;
+
+public class JobConfiguration : IEntityTypeConfiguration<Jobs>
+{
+    public void Configure(EntityTypeBuilder<Jobs> builder)
+    {
+        builder.ToTable(nameof(Jobs));
+
+        builder.HasKey(job => job.Id);
+
+        builder.Property(job => job.JobName)
+            .HasMaxLength(70)
+            .IsRequired(true);
+
+        builder.Property(job => job.WorkTime)
+            .HasMaxLength(50)
+            .IsRequired(true);
+
+        builder.Property(job => job.Salary)
+            .IsRequired(true);
+
+        builder.Property(job => job.Requirement)
+            .HasMaxLength(300)
+            .IsRequired(true);
+
+        builder.HasOne(job => job.Factories)
+            .WithMany(factory => factory.Jobs)
+            .HasForeignKey(job => job.FactoryId);
+    }
+}
