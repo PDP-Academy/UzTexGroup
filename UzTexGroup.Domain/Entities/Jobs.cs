723b9d5fd5435d86107b2cf17947979a32eb95fe
Mukhammadyor <muxammadyorkenjayev21@gmail.com>
2023-01-24 17:07:37 +0500

    Added Jobs

---
+++ UzTexGroup.Domain/Entities/Jobs.cs
@@ -0,0 +1,12 @@
+﻿namespace UzTexGroup.Domain.Entities;
+
+public class Jobs
+{
+    public Guid Id { get; set; }
+    public string JobName { get; set; }
+    public string WorkTime { get; set; }
+    public decimal Salary { get; set; }
+    public string Requirement { get; set; }
+    public Guid FactoryId { get; set; }
+    public Factory Factories { get; set; }
+}
