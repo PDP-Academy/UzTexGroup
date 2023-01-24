db95fc0ee5727dcd80c9965e77ac2b5f5e0751df
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2023-01-24 18:02:33 +0500

    application has been added

---
+++ UzTexGroup.Domain/Entities/Application.cs
@@ -0,0 +1,15 @@
+﻿namespace UzTexGroup.Domain.Entities;
+
+public class Application
+{
+    public Guid Id { get; set; }
+    public string CandidateFullName { get; set; }
+    public string TellNumber { get; set; }
+    public string Email { get; set; }
+    public string CandidateMessage { get; set; }
+    public Guid JobId { get; set; }
+    public Jobs? Job { get; set; }
+    public Guid AddressId { get; set; }
+    public Address? Address { get; set; }
+
+}
