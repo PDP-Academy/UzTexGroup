3e9a23d732c1b1d68b8b7d8bd0053ca9cfbedb5c
Diyorbek25 <diyorbek2506@gmail.com>
2023-01-25 01:00:31 +0000

    Generic Repository Added

---
+++ UzTexGroup.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using UzTexGroup.Infrastructure.DbContexts;
+
+namespace UzTexGroup.Infrastructure.Repositories;
+
+public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey>
+    where TEntity : class
+{
+    private readonly UzTexGroupDbContext appDbContext;
+
+    public GenericRepository(UzTexGroupDbContext appDbContext) =>
+        this.appDbContext = appDbContext;
+    
+    public async ValueTask<TEntity> InsertAsync(TEntity entity)
+    {
+        var entityEntry = await this.appDbContext
+            .AddAsync<TEntity>(entity);
+
+        await this.SaveChangesAsync();
+
+        return entityEntry.Entity;
+    }
+
+    public async ValueTask<TEntity> UpdateAsync(TEntity entity)
+    {
+        var entityEntry = this.appDbContext
+            .Update<TEntity>(entity);
+
+        await this.SaveChangesAsync();
+
+        return entityEntry.Entity;
+    }
+
+    public IQueryable<TEntity> SelectAll() =>
+        this.appDbContext.Set<TEntity>();
+
+    public async ValueTask<TEntity> SelectByIdAsync(TKey id) =>
+        await this.appDbContext.Set<TEntity>().FindAsync(id);
+
+    public async ValueTask<TEntity> SelectByIdWithDetailsAync(
+        Expression<Func<TEntity, bool>> expression,
+        string[] includes)
+    {
+        IQueryable<TEntity> entities = this.SelectAll();
+
+        foreach (var include in includes)
+        {
+            entities = entities.Include(include);
+        }
+
+        return await entities.FirstOrDefaultAsync(expression);
+    }
+
+    public async ValueTask<TEntity> DeleteAsync(TEntity entity)
+    {
+        var entityEntry = this.appDbContext
+            .Set<TEntity>()
+            .Remove(entity);
+
+        await this.SaveChangesAsync();
+
+        return entityEntry.Entity;
+    }
+    public async ValueTask<int> SaveChangesAsync() =>
+        await this.appDbContext.SaveChangesAsync();
+}
---
+++ UzTexGroup.Infrastructure/Repositories/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace UzTexGroup.Infrastructure.Repositories;
+
+public interface IGenericRepository<TEntity, TKey>
+{
+    ValueTask<TEntity> InsertAsync(TEntity entity);
+    ValueTask<TEntity> UpdateAsync(TEntity entity);
+    IQueryable<TEntity> SelectAll();
+    ValueTask<TEntity> SelectByIdAsync(TKey id);
+    
+    ValueTask<TEntity> SelectByIdWithDetailsAync(
+        Expression<Func<TEntity, bool>> expression,
+        string[] includes);
+
+    ValueTask<TEntity> DeleteAsync(TEntity entity);
+    ValueTask<int> SaveChangesAsync();
+}
