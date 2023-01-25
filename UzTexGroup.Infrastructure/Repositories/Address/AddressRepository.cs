8173f971e8bb74bb161f49f16ffd3e6947195290
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2023-01-25 17:00:17 +0500

    addressRepository and IAdress Repository have been added

---
+++ UzTexGroup.Infrastructure/Repositories/Address/AddressRepository.cs
@@ -0,0 +1,13 @@
+﻿using UzTexGroup.Domain.Entities;
+using UzTexGroup.Infrastructure.DbContexts;
+
+namespace UzTexGroup.Infrastructure.Repositories;
+
+public class AddressRepository : GenericRepository<Address,Guid>, IAddressRepository
+{
+	public AddressRepository(UzTexGroupDbContext appDbContext)
+		: base(appDbContext)
+	{
+
+	}
+}
---
+++ UzTexGroup.Infrastructure/Repositories/Address/IAddressRepository.cs
@@ -0,0 +1,7 @@
+﻿using UzTexGroup.Domain.Entities;
+
+namespace UzTexGroup.Infrastructure.Repositories;
+
+public interface IAddressRepository : IGenericRepository<Address, Guid>
+{
+}
