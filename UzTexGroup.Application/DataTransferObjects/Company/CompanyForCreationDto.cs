0b62a587f10c893fb708c025b7052851353736d5
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-25 18:31:20 +0500

    added creation dto for Company

---
+++ UzTexGroup.Application/DataTransferObjects/Company/CompanyForCreationDto.cs
@@ -0,0 +1,15 @@
+﻿namespace UzTexGroup.Application.DataTransferObjects.Company;
+public record CompanyForCreationDto(
+    string companyName,
+    int? employeeCount,
+    int? exportingCountriesCount,
+    string titleUz,
+    string? titleUzc,
+    string? titleEn,
+    string? titleRu,
+    string imageUrl,
+    string aboutCompanyUz,
+    string? aboutCompanyUzc,
+    string? aboutCompanyEn,
+    string? aboutCompanyRu);
+
