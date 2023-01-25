829f9d81d2f75ab91e282d64bd7dd48c99ed5b73
Diyorbek25 <diyorbek2506@gmail.com>
2023-01-25 16:40:13 +0000

    User Repository added

---
+++ UzTexGroup.Infrastructure/Repositories/User/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using UzTexGroup.Domain.Entities;
+
+namespace UzTexGroup.Infrastructure.Repositories;
+
+public interface IUserRepository : IGenericRepository<User, Guid>
+{
+
+}
---
+++ UzTexGroup.Infrastructure/Repositories/User/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using UzTexGroup.Domain.Entities;
+using UzTexGroup.Infrastructure.DbContexts;
+
+namespace UzTexGroup.Infrastructure.Repositories;
+
+public class UserRepository : GenericRepository<User, Guid>, IUserRepository
+{
+	public UserRepository(UzTexGroupDbContext appDbContext)
+		: base(appDbContext)
+	{
+	}
+}
