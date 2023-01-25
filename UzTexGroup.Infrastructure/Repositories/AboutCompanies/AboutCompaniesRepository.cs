dce813e42eec650ea4e3085956f67b242d34396e
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2023-01-25 16:59:48 +0500

    AboutCompanirepoes and IAboutCompanies repo and interface has been added

---
+++ UzTexGroup.Infrastructure/Repositories/AboutCompanies/AboutCompaniesRepository.cs
@@ -0,0 +1,14 @@
+﻿using UzTexGroup.Domain.Entities;
+using UzTexGroup.Infrastructure.DbContexts;
+
+namespace UzTexGroup.Infrastructure.Repositories;
+
+public class AboutCompaniesRepository 
+    : GenericRepository<AboutCompanies,Guid>,IAboutCompaniesRepository
+{
+	public AboutCompaniesRepository(UzTexGroupDbContext appDbContext)
+		: base(appDbContext)
+	{
+
+	}
+}
---
+++ UzTexGroup.Infrastructure/Repositories/AboutCompanies/IAboutCompaniesRepository.cs
@@ -0,0 +1,7 @@
+﻿using UzTexGroup.Domain.Entities;
+
+namespace UzTexGroup.Infrastructure.Repositories;
+
+public interface IAboutCompaniesRepository : IGenericRepository<AboutCompanies,Guid>
+{
+}
