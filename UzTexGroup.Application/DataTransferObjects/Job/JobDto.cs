44b257e1693e5a40a01a218f167c37a4c369cd4f
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2023-01-25 19:22:32 +0500

    DTOs have been added

---
+++ UzTexGroup.Application/DataTransferObjects/Job/JobDto.cs
@@ -0,0 +1,12 @@
+﻿using UzTexGroup.Application.DataTransferObjects.Factory;
+
+namespace UzTexGroup.Application.DataTransferObjects.Job;
+
+public record JobDto(
+    Guid jobId,
+    string jobName,
+    string workTime,
+    decimal salary,
+    string requirement,
+    Guid factoryId,
+    FactoryDto factory);
---
+++ UzTexGroup.Application/DataTransferObjects/Job/JobForCreationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UzTexGroup.Application.DataTransferObjects.Job;
+
+public record JobForCreationDto(
+    string jobName,
+    string workTime,
+    decimal salary,
+    string requirement,
+    Guid factoryId);
+
