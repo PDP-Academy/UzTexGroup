c7599b67ecdd99969ee2287c5f2e0cec776ccb4d
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-25 12:38:54 +0500

    added newImages configuration to Infrastructure layer

---
+++ UzTexGroup.Infrastructure/Configurations/NewImagesConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UzTexGroup.Domain.Constants;
+using UzTexGroup.Domain.Entities;
+
+namespace UzTexGroup.Infrastructure.Configurations;
+public sealed class NewImagesConfiguration : IEntityTypeConfiguration<NewImages>
+{
+    public void Configure(EntityTypeBuilder<NewImages> builder)
+    {
+        builder.ToTable(TableNames.NewImages);
+
+        builder.HasKey(newImage => newImage.Id);
+
+        builder
+            .Property(newImage => newImage.ImageUrl)
+            .HasMaxLength(256)
+            .IsRequired(true);
+
+        builder
+            .HasOne(newImage => newImage.News)
+            .WithMany(@new => @new.NewImages)
+            .HasForeignKey(newImage => @newImage.NewsId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
