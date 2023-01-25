7bb7180bdaace297119e4cd9510a5cb41130eb9a
Diyorbek25 <diyorbek2506@gmail.com>
2023-01-25 15:26:35 +0000

    fixed entities

--- UzTexGroup.Domain/Constants/TableNames.cs
+++ UzTexGroup.Domain/Constants/TableNames.cs
@@ -1,12 +1,12 @@
 ﻿namespace UzTexGroup.Domain.Constants;
 public static class TableNames
 {
-    public static string User = "Users";
+    public static string Users = "Users";
     public static string AboutCompanies = "AboutCompanies";
     public static string Company = "Company";
     public static string Address = "Adress";
-    public static string Application = "Applications";
-    public static string Factory = "Factories";
+    public static string Applications = "Applications";
+    public static string Factories = "Factories";
     public static string FactoryImages = "FactoryImages";
     public static string Jobs = "Jobs";
     public static string NewImages = "NewImages";
