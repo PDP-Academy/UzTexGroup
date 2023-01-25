d4832d16d1d2ebbfeb5ee122116bc33c25d10506
Diyorbek25 <diyorbek2506@gmail.com>
2023-01-25 00:03:57 +0000

    Added language to Properties

--- UzTexGroup.Domain/Entities/News.cs
+++ UzTexGroup.Domain/Entities/News.cs
@@ -3,8 +3,14 @@ public class News
 {
     public Guid Id { get; set; }
     public DateTime Date { get; set; }
-    public string Title { get; set; }
-    public string Body { get; set; }
+    public string TitleUz { get; set; }
+    public string TitleUzC { get; set; }
+    public string TitleEn { get; set; }
+    public string TitleRu { get; set; }
+    public string BodyUz { get; set; }
+    public string BodyUzC { get; set; }
+    public string BodyEn { get; set; }
+    public string BodyRu { get; set; }
     public ICollection<NewImages>? NewImages { get; set; }
 
 }
