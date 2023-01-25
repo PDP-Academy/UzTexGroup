24f9d68b3d94e892066c3372d1c6cb73aba0a663
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-25 16:50:25 +0500

    Added Job Repo to Infra layer

---
+++ UzTexGroup.Infrastructure/Repositories/Job/JobRepository.cs
@@ -0,0 +1,10 @@
+﻿using UzTexGroup.Domain.Entities;
+using UzTexGroup.Infrastructure.DbContexts;
+
+namespace UzTexGroup.Infrastructure.Repositories;
+public class JobRepository : GenericRepository<Jobs, Guid>, IJobRepository
+{
+	public JobRepository(UzTexGroupDbContext appDbContext) : base(appDbContext)
+	{
+	}
+}
