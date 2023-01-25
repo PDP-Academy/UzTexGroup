030967ffc602226a14fa74bf222ad636bc4063d1
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-25 16:50:05 +0500

    added company Repo to Infrastructure layer

---
+++ UzTexGroup.Infrastructure/Repositories/Company/CompanyRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using UzTexGroup.Domain.Entities;
+using UzTexGroup.Infrastructure.DbContexts;
+
+namespace UzTexGroup.Infrastructure.Repositories;
+public class CompanyRepository : GenericRepository<Company, Guid>, ICompanyRepository
+{
+	public CompanyRepository(UzTexGroupDbContext appContext) 
+		: base(appContext)
+	{
+	}
+}
