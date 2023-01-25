84c0d66993381fef824de51a18b080f8156a4c10
Jasurbek <mamatqulovjasurbek805@gmail.com>
2023-01-25 17:21:20 +0500

    Add News Repository

---
+++ UzTexGroup.Infrastructure/Repositories/NewsRepo/NewsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UzTexGroup.Infrastructure.Repositories.News
+{
+    internal class NewsRepository
+    {
+    }
+}
