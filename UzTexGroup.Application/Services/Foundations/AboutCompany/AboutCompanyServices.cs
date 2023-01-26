f8550125a1e2dbcb40913982ba344760fc795165
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-26 10:25:47 +0500

    added about company services to services layer

---
+++ UzTexGroup.Application/Services/Foundations/AboutCompany/AboutCompanyServices.cs
@@ -0,0 +1,37 @@
+﻿using UzTexGroup.Domain.Entities;
+using UzTexGroup.Infrastructure.Repositories;
+
+namespace UzTexGroup.Application.Services.Foundations.AboutCompany;
+public class AboutCompanyServices : IAboutCompanyServices
+{
+    private readonly IAboutCompaniesRepository aboutCompaniesRepository;
+    public AboutCompanyServices(IAboutCompaniesRepository aboutCompaniesRepository)
+    {
+        this.aboutCompaniesRepository = aboutCompaniesRepository;
+    }
+
+    public ValueTask<AboutCompanies> CreateUserAsync(AboutCompanies aboutCompany)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<AboutCompanies> ModifyUserAsync(AboutCompanies aboutCompany)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<AboutCompanies> RemoveUserAsync(Guid aboutCompanyId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<AboutCompanies> RetrieveUserByIdAsync(Guid aboutCompanyId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<AboutCompanies> RetrieveUsers()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ UzTexGroup.Application/Services/Foundations/AboutCompany/IAboutCompanyServices.cs
@@ -0,0 +1,11 @@
+﻿using UzTexGroup.Domain.Entities;
+
+namespace UzTexGroup.Application.Services.Foundations.AboutCompany;
+public interface IAboutCompanyServices
+{
+    ValueTask<AboutCompanies> CreateUserAsync(AboutCompanies aboutCompany);
+    IQueryable<AboutCompanies> RetrieveUsers();
+    ValueTask<AboutCompanies> RetrieveUserByIdAsync(Guid aboutCompanyId);
+    ValueTask<AboutCompanies> ModifyUserAsync(AboutCompanies aboutCompany);
+    ValueTask<AboutCompanies> RemoveUserAsync(Guid aboutCompanyId);
+}
