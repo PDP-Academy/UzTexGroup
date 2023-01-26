8503d6e587a648e980381c0b3ee575fcb248e653
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-26 10:24:58 +0500

    added About Company Dto to Data Tranfer Object folder

---
+++ UzTexGroup.Application/Services/Processings/DataTransferObjects/AboutCompany/AboutCompanyForModificationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace UzTexGroup.Application.Services.Processings.DataTransferObjects.AboutCompany;
+public record AboutCompanyForModificationDto(
+    string? titleUz,
+    string? titleUzC,
+    string? titleEn,
+    string? titleRu,
+    string? imageUrl,
+    string? aboutCompanyUz,
+    string? aboutCompanyUzC,
+    string? aboutCompanyEn,
+    string? aboutCompanyRu);
