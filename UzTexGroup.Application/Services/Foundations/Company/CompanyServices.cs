f9fe373b144e9f08f3d31781de5a633e622e390c
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-26 10:25:18 +0500

    added company services to services layer

---
+++ UzTexGroup.Application/Services/Foundations/Company/CompanyServices.cs
@@ -0,0 +1,36 @@
+﻿using UzTexGroup.Infrastructure.Repositories;
+
+namespace UzTexGroup.Application.Services.Foundations.Company;
+public class CompanyServices : ICompanyServices
+{
+    private readonly ICompanyRepository companyRepository;
+    public CompanyServices(ICompanyRepository companyRepository)
+    {
+        this.companyRepository = companyRepository;
+    }
+
+    public ValueTask<Domain.Entities.Company> CreateUserAsync(Domain.Entities.Company company)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<Domain.Entities.Company> ModifyUserAsync(Domain.Entities.Company company)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<Domain.Entities.Company> RemoveUserAsync(Guid companyId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<Domain.Entities.Company> RetrieveUserByIdAsync(Guid companyId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<Domain.Entities.Company> RetrieveUsers()
+    {
+        throw new NotImplementedException();
+    }
+}
