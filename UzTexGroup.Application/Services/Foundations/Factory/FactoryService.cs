816294cce8388b314781de82e58cd2c83787c211
Jasurbek <mamatqulovjasurbek805@gmail.com>
2023-01-27 11:24:16 +0500

    Add Factory Service Implemented

---
+++ UzTexGroup.Application/Services/Foundations/Factory/FactoryService.cs
@@ -0,0 +1,46 @@
+﻿using UzTexGroup.Infrastructure.Repositories;
+
+namespace UzTexGroup.Application.Services.Foundations.Factory
+{
+    public class FactoryService : IFactoryService
+    {
+        private readonly IFactoryReposytory factoryRepository;
+
+        public FactoryService(IFactoryReposytory factoryRepository)
+        {
+            this.factoryRepository = factoryRepository;
+        }
+
+        public async ValueTask<Domain.Entities.Factory> CreateFactoryAsync(Domain.Entities.Factory factory)
+        {
+           return await factoryRepository.InsertAsync(factory);
+        }
+
+        public async ValueTask<Domain.Entities.Factory> ModifyFactoryAsync(Domain.Entities.Factory factory)
+        {
+           return await factoryRepository.UpdateAsync(factory);
+        }
+
+        public async ValueTask<Domain.Entities.Factory> RemoveFactoryAsync(Domain.Entities.Factory factory)
+        {
+            return await factoryRepository.DeleteAsync(factory);
+        }
+
+        public async ValueTask<Domain.Entities.Factory> RetrieveByIdWithDetailsAync(Guid FactoryId)
+        {
+            return await factoryRepository.SelectByIdWithDetailsAync(
+                factory => factory.Id == FactoryId,
+                new string[] {"FactoryImages","Jobs"});
+        }
+
+        public async ValueTask<Domain.Entities.Factory> RetrieveFactoryByIdAsync(Guid FactoryId)
+        {
+            return await factoryRepository.SelectByIdAsync(FactoryId);
+        }
+
+        public IQueryable<Domain.Entities.Factory> RetrieveFactorys()
+        {
+            return factoryRepository.SelectAll();
+        }
+    }
+}
---
+++ UzTexGroup.Application/Services/Foundations/Factory/IFactoryService.cs
@@ -0,0 +1,12 @@
+﻿namespace UzTexGroup.Application.Services.Foundations.Factory
+{
+    public interface IFactoryService
+    {
+        ValueTask<Domain.Entities.Factory> CreateFactoryAsync(Domain.Entities.Factory Factory);
+        IQueryable<Domain.Entities.Factory> RetrieveFactorys();
+        ValueTask<Domain.Entities.Factory> RetrieveFactoryByIdAsync(Guid FactoryId);
+        ValueTask<Domain.Entities.Factory> ModifyFactoryAsync(Domain.Entities.Factory Factory);
+        ValueTask<Domain.Entities.Factory> RemoveFactoryAsync(Domain.Entities.Factory Factory);
+        ValueTask<Domain.Entities.Factory> RetrieveByIdWithDetailsAync(Guid FactoryId);
+    }
+}
